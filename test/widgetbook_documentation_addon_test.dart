08814524c5452a70bea14665699bcc004d2c8118
Benoît FONTAINE <benoit.fontaine@septeo.com>
2025-02-10 17:46:49 +0100

    v1.0.0

---
+++ test/widgetbook_documentation_addon_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:widgetbook_documentation_addon/widgetbook_documentation_addon.dart';
+
+void main() {
+  test('adds one to input values', () {
+  });
+}
